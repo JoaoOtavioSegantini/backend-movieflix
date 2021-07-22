deb5ca43c4ff44aba5b9635fc146ea53b5bd4be2
JoaoOtavioSegantini <joaotavioos@outlook.com>
2021-07-21 21:32:17 -0300

    Finish GenreService

--- backend/src/main/java/com/devsuperior/movieflix/resources/GenreResources.java
+++ backend/src/main/java/com/devsuperior/movieflix/resources/GenreResources.java
@@ -1,27 +1,26 @@
 package com.devsuperior.movieflix.resources;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.movieflix.entities.Genre;
+import com.devsuperior.movieflix.services.GenreService;
 
 @RestController
 @RequestMapping(value = "/genres")
 public class GenreResources {
 	
+	@Autowired
+	private GenreService service;
+	
 	@GetMapping
 	public ResponseEntity<List<Genre>> findAll() {
-		List<Genre> list = new ArrayList<>();
-		list.add(new Genre(1L, "Ação"));
-		list.add(new Genre(2L, "Aventura"));
-		list.add(new Genre(3L, "Drama"));
-		list.add(new Genre(4L, "Mistério"));
-		list.add(new Genre(5L, "Suspense"));
+		List<Genre> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 
